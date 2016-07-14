a28c0ec6b73bb77fa959eef2e2ed5b87ca7326b2
Scott Havens <roamin.grizzly@gmail.com>
2016-07-14 06:52:12 -0600

    init for smrf change

--- smrf/__init__.py
+++ smrf/__init__.py
@@ -6,12 +6,12 @@ __author__ = 'Scott Havens'
 __email__ = 'scotthavens@ars.usda.gov'
 __version__ = '0.1.0'
 
-from smrf import data
-from smrf import distribute
-from smrf import envphys
-from smrf import ipw
-from smrf import framework
-from smrf import spatial
-from smrf import utils
-from smrf import model
-from smrf import output
+import data
+import distribute
+import envphys
+import ipw
+import framework
+import spatial
+import utils
+import model
+import output
