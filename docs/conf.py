9e7ddc4cb0cb5258b6b432a7a152be6827db3401
Scott Havens <roamin.grizzly@gmail.com>
2016-07-15 06:54:39 -0600

    removed detrended_kriging

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ class Mock(MagicMock):
     @classmethod
     def __getattr__(cls, name):
             return Mock()
-MOCK_MODULES = ['netCDF4', 'matplotlib', 'matplotlib.pyplot', 'pandas']
+MOCK_MODULES = ['netCDF4', 'matplotlib', 'matplotlib.pyplot', 'pandas', 'detrended_kriging', 'smrf.spatial.dk.detrended_kriging']
 
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
--- docs/smrf.spatial.dk.rst
+++ docs/smrf.spatial.dk.rst
@@ -1,13 +1,7 @@
 smrf.spatial.dk package
 =======================
 
-smrf.spatial.dk.detrended_kriging module
-----------------------------------------
 
-.. automodule:: smrf.spatial.dk.detrended_kriging
-    :members:
-    :undoc-members:
-    :show-inheritance:
 
 smrf.spatial.dk.dk module
 -------------------------
