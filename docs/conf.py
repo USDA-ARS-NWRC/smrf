f5c98a4ada4c6c939f003d824acbac0631209d56
Scott Havens <roamin.grizzly@gmail.com>
2016-07-15 07:33:42 -0600

    changed conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,11 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../'))
+
+if os.environ.get('READTHEDOCS') == 'True':
+    sys.path.insert(0, os.path.abspath('.'))
+else:
+    sys.path.insert(0, os.path.abspath('../'))
 
 # -- Have to do a mock install of some modules that RTD doesn't have --------
 from mock import Mock as MagicMock
