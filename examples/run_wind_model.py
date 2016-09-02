b384cc2a9af84daaa888084eafadbe95beeb0469
Scott Havens <scott.havens@ars.usda.gov>
2016-09-02 07:51:49 -0600

    1. Moved the run_wind_model.py to the examples folder 2. Cleaned up the original wind model code from the wind folder

--- examples/run_wind_model.py
+++ examples/run_wind_model.py
@@ -4,10 +4,18 @@ Example script for running Adam Winstral's wind model
 20160622 Scott Havens
 """
 
-import smrf
+from smrf.utils.wind.model import wind_model
+from smrf.ipw import IPW
 import netCDF4 as nc
 import matplotlib.pyplot as plt
 import numpy as np
+
+from datetime import datetime
+start = datetime.now()
+
+import faulthandler
+
+faulthandler.enable()
 
 #------------------------------------------------------------------------------ 
 # Specify model input parameters
@@ -15,9 +23,10 @@ import numpy as np
 # DEM file to read in
 dem_file = '../test_data/topo/dem30m.ipw'
 dem_file = '/home/scotthavens/Documents/Projects/smrf/test_data/topo/dem30m.ipw'
+# dem_file = '/home/scotthavens/Documents/Projects/smrf/smrf/utils/wind/ned30m_brb.ipw'
 
-# middle upwind direction around which to run model (degrees)
-angle = 180
+# # middle upwind direction around which to run model (degrees)
+# angle = 180
 
 # increment between direction calculations (degrees)
 inc = 5
@@ -29,40 +38,36 @@ dmax = 690
 sepdist = 90
 
 # Anemometer height (meters)
-inst = 2
+inst = 3
 
 # Windower
 windower = 30
 
 save_file = '/home/scotthavens/Documents/Projects/smrf/examples/smrf_maxus.nc'
+save_file2 = '/home/scotthavens/Documents/Projects/smrf/examples/smrf_tbreak.nc'
 
 #------------------------------------------------------------------------------
 # run the wind model
 
 # read in the DEM
-dem = smrf.ipw.IPW(dem_file)
+dem = IPW(dem_file)
+# dem_data = np.round(dem.bands[0].data)
 dem_data = dem.bands[0].data
 x = dem.bands[0].x
 y = dem.bands[0].y
 
-w = smrf.utils.wind_model.wind_model(x, y, dem_data)
+# initialize the wind model with the dem
+w = wind_model(x, y, dem_data, nthreads=12)
 
-w.maxus(dmax, sepdist, inc, inst, save_file)
+# calculate the maxus for the parameters and output to file
+w.maxus(dmax, inc, inst, save_file)
+print datetime.now() - start
+
+# window the maxus values based on the maxus values in the file
+w.windower(save_file, windower, 'maxus')
+
+# calculate the maxus for the parameters and output to file
+# w.tbreak(dmax, sepdist, inc, inst, save_file2)
 
 
-
-
-
-
-
-
-#------------------------------------------------------------------------------
-# compare with the original outputs
-
-m = nc.Dataset('/home/scotthavens/Documents/Projects/smrf/examples/maxus.nc')
-
-mxs = m.variables['maxus'][0,:]
-
-plt.imshow(w.maxus_val - mxs)
-plt.colorbar()
-plt.show()
+print datetime.now() - start
