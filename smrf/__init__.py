76e627779a96472dabd40cb704880c3ed7001c26
Micah Johnson <micah.johnson150@gmail.com>
2018-01-30 15:32:17 -0700

    updating smrf to work with new inicheck package

--- smrf/__init__.py
+++ smrf/__init__.py
@@ -5,6 +5,7 @@ SMRF - Snow Modeling Resource Framework
 __version__ = '0.4.9'
 import os
 __core_config__ = os.path.abspath(os.path.dirname(__file__)+'/framework/CoreConfig.ini')
+__recipes__ = os.path.abspath(os.path.dirname(__file__)+'/framework/recipes.ini')
 from . import data
 from . import distribute
 from . import envphys
