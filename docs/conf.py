3a5fc3dd1d8f805662ea8a1c46b5c6a3ff320c6f
Micah Johnson <micah.johnson150@gmail.com>
2018-07-13 10:55:56 -0600

    An attempt at fixing the documents issue with pykrig, had to make it its own repo with a fix to get past odd install issues, this is addressing #76

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ class Mock(MagicMock):
     @classmethod
     def __getattr__(cls, name):
             return Mock()
-MOCK_MODULES = ['netCDF4', 'matplotlib', 'matplotlib.pyplot', 'pandas']
+MOCK_MODULES = ['netCDF4', 'matplotlib', 'matplotlib.pyplot', 'pandas','pykrige']
 
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
