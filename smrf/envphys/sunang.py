c9009ad35291e8e5b9f8f390bae908df822f5511
Scott Havens <scotthavens42@gmail.com>
2019-10-01 16:13:06 -0600

    able to reproduce the exact numbers from IPW in Python

---
+++ smrf/envphys/sunang.py
@@ -0,0 +1,454 @@
+import numpy as np
+from math import fmod, tan, atan, cos, sin, asin, sqrt
+
+JULIAN_CENTURY = 36525		# days in Julian century
+DEGS_IN_CIRCLE = 3.6e2        # degrees in circle
+TOLERANCE = 1.1920928955e-07
+M_PI = 3.14159265358979323846
+M_PI_2 = 2 * M_PI
+MAX_DECLINATION = 23.6
+
+
+def sunang(date_time, latitude, longitude):
+    """
+    Args:
+        date_time: python datetime object
+        latitude: in degrees
+        longitude: in degrees
+
+    Returns
+        cosz, azimuth
+    """
+
+    # Calculate the ephemeris parameters
+    declination, omega, rad_vec = ephemeris(date_time)
+
+    # calculate the sun angle
+    rad_latitude = latitude * M_PI /180
+    rad_longitude = longitude * M_PI / 180
+    mu, azimuth = sunpath(rad_latitude, rad_longitude, declination, omega)
+
+    azimuth = azimuth * 180 / M_PI
+
+    return mu, azimuth, rad_vec
+
+
+def sunpath(latitude, longitude, declination, omega):
+    """
+    Args:
+        latitude: in radians
+        longitude: in radians
+        declination
+        omega
+
+    Returns
+        cosz, azimuth in radians
+    """
+
+    if np.abs(latitude) > M_PI_2:
+        raise ValueError("latitude ({} deg) not between -90 and +90 degrees".format(latitude*180/M_PI))
+
+    if np.abs(longitude) > M_PI:
+        raise ValueError("longitude ({} deg) not between -180 and +180 degrees".format(longitude*180/M_PI))
+
+    if np.abs(declination) > M_PI*MAX_DECLINATION/180:
+        raise ValueError("declination ({} deg) > maximum declination ({} deg)".format(declination*180/M_PI, MAX_DECLINATION))
+
+    if np.abs(omega) > M_PI:
+        raise ValueError("longitude of sun ({} deg) not between -180 and +180 degrees".format(omega*180/M_PI))
+
+    cosz, az = rotate(np.sin(declination), omega, np.sin(latitude), longitude)
+
+    return cosz, az
+
+def dsign(a, b):
+    """
+    modified from /usr/src/lib/libF77/d_sign.c
+    """
+    
+    x = a if a >= 0 else -a
+    y = x if b >= 0 else -x
+    return y
+
+# original ibm 360/44 fortran ivf - vislab - wilson - 29jul79
+# translated, modified and reduced by dozier - ucsb - 11/81
+
+def ephemeris(dt):
+    """
+    NAME
+        ephemeris - calculates ephemeris data
+
+    SYNOPSIS
+        #include "solar.h"
+
+        int
+        ephemeris(
+            datetime_t  *dt,	|* date-time (GMT)		 *|
+            double      *r,		|* radius vector		 *|
+            double      *declin,	|* declination (radians, +north) *|
+            double      *omega)	|* sun longitude (radians +east) *|
+
+    DESCRIPTION
+        Calculates radius vector, declination, and apparent longitude
+        of sun, as function of the given date and time.
+
+        The routine is adapted from:
+
+        W. H. Wilson, Solar ephemeris algorithm, Reference 80-13, 70
+            pp., Scripps Institution of Oceanography, University of
+            California, San Diego, La Jolla, CA, 1980.
+
+    RETURN VALUE
+        OK	calculations succeeded, and output parameters assigned.
+
+        ERROR   error occurred, and message stored via 'usrerr' routine.
+    """
+        
+    one = 1.0
+    degrd = atan(one) / 45.0
+
+    # Convert time to seconds since midnight
+    gmts = 3600.0 * dt.hour + 60.0 * dt.minute + dt.second
+
+    # p51 = gmts/3600/24*360
+
+    # The int() is required for compatability with IPW as the divide by 100 keeps
+    # the value as an integer where python converts to a float...
+    p51 = gmts / 10.0 / 24.0
+    p22 = int(((dt.year - 1900) * JULIAN_CENTURY - 25) / 100) + yearday(dt.year, dt.month, dt.day) - 0.5
+    p23 = (p51 / DEGS_IN_CIRCLE + p22) / JULIAN_CENTURY
+    p22 = p23 * JULIAN_CENTURY
+
+    # mean longitude - p24
+    p11 = 279.69668
+    p12 = 0.9856473354
+    p13 = 3.03e-4
+    p24 = p11 + fmod(p12 * p22, DEGS_IN_CIRCLE) + p13 * p23 * p23
+    p24 = fmod(p24, DEGS_IN_CIRCLE)
+
+    # mean anomaly - p25
+    p11 = 358.47583
+    p12 = 0.985600267
+    p13 = -1.5e-4
+    p14 = -3.e-6
+    p25 = p11 + fmod(p12 * p22, DEGS_IN_CIRCLE) + p23 * p23 * (p13 + p14 * p23)
+    p25 = fmod(p25, DEGS_IN_CIRCLE)
+ 
+    # eccentricity - p26
+    p11 = 0.01675104
+    p12 = -4.18e-5
+    p13 = -1.26e-7
+    p26 = p11 + p23 * (p12 + p13 * p23)
+    p11 = p25 * degrd
+    p12 = p11
+
+    while True:
+        p13 = p12
+        p12 = p11 + p26 * sin(p13)
+        if abs((p12 - p13) / p12) < TOLERANCE:
+            break
+
+    p13 = p12 / degrd
+
+    # true anomaly - p27`
+    p27 = 2.0 * atan(sqrt((1.0 + p26) / (1.0 - p26)) * tan(p13 / 2.0 * degrd)) / degrd
+    if dsign(1.0, p27) != dsign(1.0, sin(p13 * degrd)):
+        p27 += 1.8e2
+    if p27 < 0.0:
+        p27 += DEGS_IN_CIRCLE
+ 
+    # radius vector - r
+    r = 1.0 - p26 * cos(p13 * degrd)
+
+    # aberration - p29
+    p29 = -20.47 / r / 3600
+
+    # mean obliquity - p43
+    p11 = 23.452294
+    p12 = -0.0130125
+    p13 = -1.64e-6
+    p14 = 5.03e-7
+    p43 = p11 + p23 * (p12 + p23 * (p13 + p14 * p23))
+ 
+    #  mean ascension - p45
+    p11 = 279.6909832
+    p12 = 0.98564734
+    p13 = 3.8707
+    p13 = 3.8708e-4
+    p45 = p11 + fmod(p12 * p22, DEGS_IN_CIRCLE) + p13 * p23 * p23
+    p45 = fmod(p45, DEGS_IN_CIRCLE)
+
+    # nutation and longitude pert
+    p11 = 296.104608
+    p12 = 1325 * DEGS_IN_CIRCLE
+    p13 = 198.8491083
+    p14 = 0.00919167
+    p15 = 1.4388e-5
+    p28 = p11 + fmod(p12 * p23, DEGS_IN_CIRCLE) + p23 * (p13 + p23 * (p14 + p15 * p23))
+    p28 = fmod(p28, DEGS_IN_CIRCLE)
+
+    # mean elongation of moon - p30
+    p11 = 350.737486
+    p12 = 1236 * DEGS_IN_CIRCLE
+    p13 = 307.1142167
+    p14 = 1.436e-3
+    p30 = p11 + fmod(p12 * p23, DEGS_IN_CIRCLE) + p23 * (p13 + p14 * p23)
+    p30 = fmod(p30, DEGS_IN_CIRCLE)
+
+    # moon long of ascending node - p31
+    p11 = 259.183275
+    p12 = -5 * DEGS_IN_CIRCLE
+    p13 = -134.142008
+    p14 = 2.0778e-3
+    p31 = p11 + fmod(p12 * p23, DEGS_IN_CIRCLE) + p23 * (p13 + p14 * p23)
+    p31 = fmod(p31, DEGS_IN_CIRCLE)
+
+    # mean long of moon - p31
+    p11 = 270.434164
+    p12 = 1336 * DEGS_IN_CIRCLE
+    p13 = 307.8831417
+    p14 = -1.1333e-3
+    p32 = p11 + fmod(p12 * p23, DEGS_IN_CIRCLE) + p23 * (p13 + p14 * p23)
+    p32 = fmod(p32, DEGS_IN_CIRCLE)
+
+    # moon perturbation of sun long - p33
+    p33 = 6.454 * sin(p30 * degrd) + 0.013 * sin(3 * p30 * degrd) + \
+        0.177 * sin((p30 + p28) * degrd) - \
+        0.424 * sin((p30 - p28) * degrd)
+    p33 = p33 / 3600
+
+    # nutation of long - p34
+    p34 = -(17.234 - 0.017 * p23) * sin(p31 * degrd) + \
+        0.209 * sin(2 * p31 * degrd) - \
+        0.204 * sin(2 * p32 * degrd)
+    p34 = p34 - 1.257 * sin(2 * p24 * degrd) + 0.127 * sin(p28 * degrd)
+    p34 = p34 / 3600
+
+    # nutation in obliquity - p34
+    p35 = 9.214 * cos(p31 * degrd) + 0.546 * cos(2 * p24 * degrd) - \
+        .09 * cos(2 * p31 * degrd) + 0.088 * cos(2 * p32 * degrd)
+    p35 = p35 / 3600
+
+    # inequalities of long period - p36
+    p36 = 0.266 * sin((31.8 + 119 * p23) * degrd) + \
+        ((1.882 - 0.016 * p23) * degrd) * \
+        sin((57.24 + 150.27 * p23) * degrd)
+    p36 = p36 + 0.202 * sin((315.6 + 893.3 * p23) * degrd) + \
+        1.089 * p23 * p23 + 6.4 * sin((231.19 + 20.2 * p23) * degrd)
+    p36 = p36 / 3600
+
+    # apparent longitude - p41
+    p41 = p27 - p25 + p24 + p29 + p33 + p36 + p34
+
+    p43 += p35
+
+    # apparent right ascension - p44
+    p44 = atan(tan(p41 * degrd) * cos(p43 * degrd)) / degrd
+    if dsign(one, p44) != dsign(one, sin(p41 * degrd)):
+        p44 += 1.8e2
+    if p44 < 0:
+        p44 += DEGS_IN_CIRCLE
+
+    # equation of time - p46
+    p46 = p45 - p44
+    if p46 > 1.8e2:
+        p46 -= DEGS_IN_CIRCLE
+
+    # declination - p47
+    p47 = asin(sin(p41 * degrd) * sin(p43 * degrd))
+    declin = p47
+
+    # hour angle in degrees - p48
+    p48 = p51 + p46 - 1.8e2
+    if p48 > 180:
+        p48 -= DEGS_IN_CIRCLE
+    elif p48 < -180:
+        p48 += DEGS_IN_CIRCLE
+    omega = -p48 * degrd
+
+    return declin, omega, r
+
+def rotate(mu, azm, mu_r, lam_r):
+    """
+    NAME
+	rotate - rotation of spherical coordinates
+
+    SYNOPSIS
+        #include "ipw.h"
+
+        int
+        rotate(
+            double    mu,		|* cosine of angle theta from z-axis *|
+            double    azm,		|* azimuth (+ccw from x-axis) 	     *|
+            double    mu_r,		|* cosine of angle theta_r of
+                        rotation of z-axis		     *|
+            double    lam_r,	|* azimuth (+ccw) of x-axis rotation *|
+            double   *muPrime,	|* new cosine of angle from z'-axis  *|
+            double   *aPrime)	|* new azimuth from x'-axis	     *|
+
+    DESCRIPTION
+        Calculates new spherical coordinates if system rotated about
+        origin.  Coordinates are right-hand system.  All angles are in
+        radians.
+
+        input --
+        mu	cosine of angle theta from z-axis in old coordinate system
+        azm	azimuth (+ccw from x-axis) in old coordinate system
+        mu_r	cosine of angle theta_r of rotation of z-axis
+        lam_r	azimuth (+ccw) of rotation of x-axis
+
+        output --
+        muPrime	new value of cosine of angle from z'-axis
+        aPrime	new value of azimuth from x'-axis
+
+        for example, to use this program to calculate sun angles, set:
+
+        mu	= sin(declination)
+        azm	= hour angle of sun (long. where sun is vertical)
+        mu_r	= sin(latitude)
+        lam_r	= longitude
+
+        output will be muPrime = cosZ and aPrime = solar azimuth
+
+    RETURN VALUE
+        cosZ
+        solar azimuth
+    """
+
+    # Check input values: mu = cos(theta) mu_r = cos(theta-sub-r)
+    if mu < -1.0 or mu > 1.0:
+        raise ValueError("rotate: mu = cos(theta) = {} is not between -1 and +1".format(mu))
+
+    if mu_r < -1.0 or mu_r > 1.0:
+        raise ValueError("rotate: mu_rotate = cos(theta-sub-r) = {} is not between -1 and +1".format(mu_r))
+
+    if np.abs(azm) > (2 * np.pi):
+        raise ValueError("rotate: azimuth ({} deg) is not between -360 and 360".format(180 * (azm * 360) / np.pi))
+
+    if np.abs(lam_r) > (2 * np.pi):
+        raise ValueError("rotate: lam_r ({} deg) = axis rotation, is not between -360 and 360".format(180 * lam_r / np.pi))
+
+    # difference between azimuth and rotation angle of x-axis
+    omega = lam_r - azm
+
+    #sine of angle theta (cosine is an argument)
+    #sine of angle theta-sub-r (cosine is an argument)
+    sinTheta = np.sqrt((1. - mu) * (1. + mu))
+    sinThr = np.sqrt((1. - mu_r) * (1. + mu_r))
+        
+    #cosine of difference between azimuth and aziumth of rotation
+    cosOmega = np.cos(omega)
+
+    #Output:
+    #azimuth and cosine of angle in rotated axis system.
+    #(bug if trig routines trigger error)
+    aPrime = -np.arctan2(sinTheta * sin(omega),
+                mu_r * sinTheta * cosOmega - mu * sinThr)
+    muPrime = sinTheta * sinThr * cosOmega + mu * mu_r
+    
+    return muPrime, aPrime
+
+
+
+ 
+
+def yearday(year, month, day):
+    """
+    NAME
+        yearday -- returns the yearday (day of year) for a given date
+
+    SYNOPSIS
+        #include "ipw.h"
+
+        int
+        yearday(
+            int 	year,		|* e.g., 1997		*|
+            int 	month,		|* [1 - 12]		*|
+            int 	day)		|* [1 - 31]		*|
+
+    DESCRIPTION
+        yearday returns the yearday for the given date.  yearday is
+        the 'day of the year', sometimes called (incorrectly) 'julian day'.
+
+    RETURN VALUE
+        1 - 366
+    """
+
+    assert(year >= 0)
+    assert(1 <= month and month <= 12)
+    assert(1 <= day and day <= numdays(year, month))
+
+    yday = day
+
+    # Add in number of days for preceeding months.
+    for mon in range(month-1, 0, -1):
+        yday += numdays(year, mon)
+
+    return yday
+
+
+def numdays(year, month):
+    """
+     NAME
+        numdays - return number of days in a month
+    
+    SYNOPSIS
+        #include "ipw.h"
+
+        int
+        numdays(
+            int		year,		|* year			*|
+            int		month)		|* month (1 to 12)	*|
+    
+    DESCRIPTION
+        numdays retuns the number of days in the given month of
+        the given year.
+    
+    RETURN VALUE
+        number of days in month
+    """
+
+    NDAYS = list([31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31])
+
+    assert(year >= 0)
+    assert(1 <= month and month <= 12)
+
+    ndays = NDAYS[month-1]
+
+    # Check for leap year for February
+    if ((month == 2) and leapyear(year)):
+        ndays += 1
+
+    return ndays
+
+
+def leapyear(year):
+    """
+    NAME
+        leapyear -- is a given year a leap year?
+
+    SYNOPSIS
+        #include "ipw.h"
+
+        bool_t
+        leapyear(
+            int 	year)
+
+    DESCRIPTION
+        leapyear determines if the given year is a leap year or not.
+        year must be positive, and must not be abbreviated; i.e.
+        89 is 89 A.D. not 1989.
+
+    RETURN VALUE
+        TRUE	if a leap year
+
+        FALSE	if not a leap year
+    """
+
+    assert(year >= 0)
+
+    if ((year % 4 == 0 and year % 100 != 0) or year % 400 == 0):
+        return True
+    else:
+        return False
+	
--- tests/test_radiation.py
+++ tests/test_radiation.py
@@ -3,7 +3,7 @@ import unittest
 import pandas as pd
 import numpy as np
 
-from smrf.envphys import radiation
+from smrf.envphys import radiation, sunang
 from tests.test_configurations import SMRFTestCase
 
 from pysolar.solar import get_altitude, get_azimuth
@@ -28,55 +28,86 @@ class TestRadiation(SMRFTestCase):
 
         date_time = pd.to_datetime('2/15/1990 20:30')
         date_time = date_time.tz_localize('UTC')
-        lat = 34.416667
+        lat = 34.4166667 # 35d, 25m, 0s
         lon = -119.9
+        ipw_cosz = 0.680436
+        ipw_azimuth = -5.413
+        ipw_rad_vector = 0.98787
         cosz = 0.6806311288888297
         zenith = 47.107
         az = -5.416396416371043
         
-        # result = radiation.sunang(date_time, lat, lon)
+        result = radiation.sunang(date_time, lat, lon)
+        self.assertTrue(result[0] == ipw_cosz)
+        self.assertTrue(result[1] == ipw_azimuth)
+
+        # try out the python version
+        result = sunang.sunang(date_time, lat, lon)
+        self.assertTrue(round(result[0], 6), ipw_cosz)
+        self.assertTrue(round(result[1], 3), ipw_azimuth)
+        self.assertTrue(round(result[2], 5), ipw_rad_vector)
+
+
+        # result = radiation.pysolar_sunang(date_time, lat, lon)
         # self.assertTrue(result[0] == cosz)
         # self.assertTrue(result[1] == az)
 
-        result = radiation.pysolar_sunang(date_time, lat, lon)
-        self.assertTrue(result[0] == cosz)
-        self.assertTrue(result[1] == az)
+    
+    def test_sunang_functions(self):
+        """ Sunang functions """
+
+        self.assertTrue(sunang.leapyear(2016))
+        self.assertFalse(sunang.leapyear(2013))
+
+        self.assertEqual(sunang.yearday(2016, 1, 31), 31)
+        self.assertEqual(sunang.yearday(2016, 12, 31), 366)
+        self.assertEqual(sunang.yearday(2015, 12, 31), 365)
+        self.assertEqual(sunang.yearday(2015, 10, 1), 274)
+        self.assertEqual(sunang.yearday(2016, 10, 1), 275)
+
+        # replicating the IPW sunang example
+        date_time = pd.to_datetime('2/15/1990 20:30')
+        date_time = date_time.tz_localize('UTC')
+        declin, omega, r = sunang.ephemeris(date_time)
+        self.assertEqual(round(declin, 9), -0.218992538)
+        self.assertEqual(round(omega, 9), -2.163529935)
+        self.assertEqual(round(r, 9), 0.987871247)
 
 
     # The code that generated the figures in the PR for comparison
     # between the IPW version and the Pysolar version
-    # def test_sunang_timeseries(self):
-    #     """ Sunang calculation timeseries """ 
+    def test_sunang_timeseries(self):
+        """ Sunang calculation timeseries """ 
 
-    #     # RME basin lat/lon
-    #     lon = -116.7547
-    #     lat = 43.067
+        # RME basin lat/lon
+        lon = -116.7547
+        lat = 43.067
 
-    #     date_time = pd.date_range('2015-10-01 00:00', '2016-09-30 00:00', freq='H', tz='UTC')
+        date_time = pd.date_range('2015-10-01 00:00', '2016-09-30 00:00', freq='H', tz='UTC')
         
-    #     df = pd.DataFrame(
-    #         index=date_time,
-    #         columns=['ipw_cosz', 'ipw_az', 'pysolar_cosz', 'pysolar_az']
-    #         )
+        df = pd.DataFrame(
+            index=date_time,
+            columns=['ipw_cosz', 'ipw_az', 'py_cosz', 'py_az']
+            )
 
-    #     for dt in date_time:
-    #         print(dt)
-    #         result = radiation.sunang(dt, lat, lon)
-    #         df.loc[dt, ['ipw_cosz', 'ipw_az']] = result
+        for dt in date_time:
+            print(dt)
+            result = radiation.sunang(dt, lat, lon)
+            df.loc[dt, ['ipw_cosz', 'ipw_az']] = result
 
-    #         result = radiation.pysolar_sunang(dt, lat, lon)
-    #         df.loc[dt, ['pysolar_cosz', 'pysolar_az']] = result
+            result = sunang.sunang(dt, lat, lon)
+            df.loc[dt, ['py_cosz', 'py_az']] = result
 
-    #     df['zen_diff'] = (df['ipw_cosz'] - df['pysolar_cosz']) * 180 / np.pi
-    #     df['az_diff'] = df['ipw_az'] - df['pysolar_az']
+        df['zen_diff'] = (df['ipw_cosz'] - df['py_cosz']) * 180 / np.pi
+        df['az_diff'] = df['ipw_az'] - df['py_az']
 
-    #     df.to_csv('sunang_comparison.csv')
+        df.to_csv('sunang_comparison.csv')
 
-    #     ax = df['zen_diff'].hist(bins=50)
-    #     ax.set_title('IPW zenith - Pysolar zenith')
-    #     ax.set_xlabel('Zenith difference, degrees')
+        ax = df['zen_diff'].hist(bins=50)
+        ax.set_title('IPW zenith - Python zenith')
+        ax.set_xlabel('Zenith difference, degrees')
         
-    #     ax = df['az_diff'].hist(bins=50)
-    #     ax.set_title('IPW azimuth - Pysolar azimuth')
-    #     ax.set_xlabel('Azimuth difference, degrees')
+        ax = df['az_diff'].hist(bins=50)
+        ax.set_title('IPW azimuth - Python azimuth')
+        ax.set_xlabel('Azimuth difference, degrees')
         
