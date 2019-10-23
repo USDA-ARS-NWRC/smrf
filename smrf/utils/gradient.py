d05f81d637d90ad859da1c7342cb9c1fe620e1f2
Scott Havens <scotthavens42@gmail.com>
2019-10-23 12:20:57 -0600

    Added better tests for gradient and aspect along with a conversion from standard degrees to ipw radians

--- smrf/utils/gradient.py
+++ smrf/utils/gradient.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def gradient_d4(dem, dx, dy):
+def gradient_d4(dem, dx, dy, aspect_rad=False):
     """
     Calculate the slope and aspect for provided dem,
     this will mimic the original IPW gradient method that
@@ -26,9 +26,11 @@ def gradient_d4(dem, dx, dy):
         dem: array of elevation values
         dx: cell size along the x axis
         dy: cell size along the y axis
+        aspect_rad: turn the aspect from degrees to IPW radians
 
     Returns:
         slope in radians
+        aspect in degrees or IPW radians
     """
 
     # Pad the dem
@@ -38,7 +40,7 @@ def gradient_d4(dem, dx, dy):
     dem_pad[0, :] = dem_pad[1, :] + (dem_pad[1, :] - dem_pad[2, :])
 
     # bottom
-    dem_pad[-1, :] = dem_pad[-2, :] + (dem_pad[-2, :] - dem_pad[-1, :])
+    dem_pad[-1, :] = dem_pad[-2, :] + (dem_pad[-2, :] - dem_pad[-3, :])
 
     # left
     dem_pad[:, 0] = dem_pad[:, 1] + (dem_pad[:, 1] - dem_pad[:, 2])
@@ -55,10 +57,13 @@ def gradient_d4(dem, dx, dy):
     slope = np.arctan(np.sqrt(dz_dx**2 + dz_dy**2))
     a = aspect(dz_dx, dz_dy)
 
+    if aspect_rad:
+        a = aspect_to_ipw_radians(a)
+
     return slope, a
 
 
-def gradient_d8(dem, dx, dy):
+def gradient_d8(dem, dx, dy, aspect_rad=False):
     """
     Calculate the slope and aspect for provided dem,
     using a 3x3 cell around the center
@@ -83,9 +88,11 @@ def gradient_d8(dem, dx, dy):
         dem: array of elevation values
         dx: cell size along the x axis
         dy: cell size along the y axis
+        aspect_rad: turn the aspect from degrees to IPW radians
 
     Returns:
         slope in radians
+        aspect in degrees or IPW radians
     """
 
     # Pad the dem
@@ -95,7 +102,7 @@ def gradient_d8(dem, dx, dy):
     dem_pad[0, :] = dem_pad[1, :] + (dem_pad[1, :] - dem_pad[2, :])
 
     # bottom
-    dem_pad[-1, :] = dem_pad[-2, :] + (dem_pad[-2, :] - dem_pad[-1, :])
+    dem_pad[-1, :] = dem_pad[-2, :] + (dem_pad[-2, :] - dem_pad[-3, :])
 
     # left
     dem_pad[:, 0] = dem_pad[:, 1] + (dem_pad[:, 1] - dem_pad[:, 2])
@@ -115,24 +122,55 @@ def gradient_d8(dem, dx, dy):
     slope = np.arctan(np.sqrt(dz_dx**2 + dz_dy**2))
     a = aspect(dz_dx, dz_dy)
 
+    if aspect_rad:
+        a = aspect_to_ipw_radians(a)
+
     return slope, a
 
 
 def aspect(dz_dx, dz_dy):
     """
     Calculate the aspect from the finite difference.
-    Aspect is radians from south (aspect 0 is toward
-    the south) with range from -pi to pi, with negative 
-    values to the west and positive values to the east
+    Aspect is degrees clockwise from North (0/360 degrees)
+
+    See below for a referance to how ArcGIS calculates slope
+    http://help.arcgis.com/en/arcgisdesktop/10.0/help/index.html#/How_Aspect_works/00q900000023000000/
 
     Args:
         dz_dx: finite difference in the x direction
         dz_dy: finite difference in the y direction
 
     Returns
-        aspect in radians
+        aspect in degrees
     """
 
-    a = np.arctan2(-dz_dy, -dz_dx)
+    # return in degrees
+    a = 180 * np.arctan2(dz_dy, -dz_dx) / np.pi
 
-    return a
+    aout = 90 - a
+    aout[a < 0] = 90 - a[a < 0]
+    aout[a > 90] = 360 - a[a > 90] + 90
+
+    return aout
+
+
+def aspect_to_ipw_radians(a):
+    """
+    IPW defines aspect differently than most GIS programs
+    so convert an aspect in degrees from due North (0/360)
+    to the IPW definition.
+
+    Aspect is radians from south (aspect 0 is toward
+    the south) with range from -pi to pi, with negative 
+    values to the west and positive values to the east
+
+    Args:
+        a: aspect in degrees from due North
+
+    Returns
+        a: aspect in radians from due South
+    """
+
+    arad = np.pi - a * np.pi / 180
+
+    return arad
