2eed9fc44960c3e31fd424c1c55976e6d061e847
Micah Johnson <micah.johnson150@gmail.com>
2019-10-28 10:06:14 -0600

    FIxed test_configurations.py

--- tests/test_configurations.py
+++ tests/test_configurations.py
@@ -20,31 +20,33 @@ class SMRFTestCase(unittest.TestCase):
         config_file = 'test_base_config.ini'
         if os.path.isfile(config_file):
             self.test_dir = ''
+
         elif os.path.isfile(os.path.join('tests', config_file)):
             config_file = os.path.join('tests', config_file)
             self.test_dir = 'tests'
+
         else:
             raise Exception('Configuration file not found for testing')
 
         self.config_file = config_file
 
         # read in the base configuration
-        self.base_config = get_user_config(config_file, modules = 'smrf')
+        self.base_config = get_user_config(config_file, modules='smrf')
 
-    def tearDown(self):
-        """
-        Clean up the output directory
-        """
-
-        folder = os.path.join(self.test_dir, 'RME', 'output')
-        for the_file in os.listdir(folder):
-            file_path = os.path.join(folder, the_file)
-            try:
-                if os.path.isfile(file_path):
-                    os.unlink(file_path)
-                elif os.path.isdir(file_path): shutil.rmtree(file_path)
-            except Exception as e:
-                print(e)
+    # def tearDown(self):
+    #     """
+    #     Clean up the output directory
+    #     """
+    #
+    #     folder = os.path.join(self.test_dir, 'RME', 'output')
+    #     for the_file in os.listdir(folder):
+    #         file_path = os.path.join(folder, the_file)
+    #         try:
+    #             if os.path.isfile(file_path):
+    #                 os.unlink(file_path)
+    #             elif os.path.isdir(file_path): shutil.rmtree(file_path)
+    #         except Exception as e:
+    #             print(e)
 
 
 
@@ -54,11 +56,9 @@ class TestConfigurations(SMRFTestCase):
         """
         Test the config for running configurations with different options
         """
-
         # test the base run with the config file
         result = can_i_run_smrf(self.config_file)
         self.assertTrue(result)
-#         self.assertTrue(False)
 
         # test the base run with the config file
         result = can_i_run_smrf(self.base_config)
