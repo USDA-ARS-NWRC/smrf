d9f939ce07ba0dcb7d148a13c47182bf6505c9d8
Scott Havens <scotthavens42@gmail.com>
2020-06-05 09:23:41 -0600

    gold compare script

--- tests/RME/plot_gold_difference_to_master.py
+++ tests/RME/plot_gold_difference_to_master.py
@@ -1,71 +1,47 @@
 #
 # Script to plot difference in gold files between current branch and master
+# Must be ran from the project root directory
 #
 
 import os
-from glob import glob
-from pathlib import PurePath
+from goldmeister.compare import GoldGitBranchCompare
 
-import matplotlib.pyplot as plt
-import netCDF4
-import numpy
-import pygit2
-
-import smrf
-
-SMRF_FOLDER = os.path.abspath(os.path.dirname(smrf.__file__) + '/..')
-GOLD_FOLDERS = ['gold', 'gold_hrrr']
-
-SMRF_REPO = pygit2.Repository(SMRF_FOLDER)
-CURRENT_BRANCH = SMRF_REPO.head.name
-MASTER_BRANCH = SMRF_REPO.branches['master']
-
-LABEL = "Min: {:.3E}\nMax: {:.3E}\nMean: {:.3E}"
+files = [
+    'air_temp',
+    'net_solar',
+    'percent_snow',
+    'precip_temp',
+    'precip',
+    'snow_density',
+    'thermal',
+    'vapor_pressure',
+    'wind_direction',
+    'wind_speed'
+]
 
 
-def get_file_values(branch, file):
-    SMRF_REPO.checkout(branch)
-    print(f"Getting data from {SMRF_REPO.head.name} for {file}")
+def main():
 
-    branch_data = netCDF4.Dataset(file)
-    variable_data = [
-        branch_data[variable][:] for variable
-        in branch_data.variables.keys()
-        if variable not in ['x', 'y', 'time', 'projection']
+    repo_path = os.getcwd()
+
+    gold_files = [
+        os.path.join(repo_path, 'tests', 'RME', 'gold', f"{gf}.nc") for gf in files
     ]
-    branch_data.close()
-    return variable_data[0].flatten()
 
+    gc = GoldGitBranchCompare(
+        repo_path=repo_path,
+        gold_files=gold_files,
+        file_type='netcdf',
+        old_branch='upstream/master',
+        new_branch='origin/156_topocalc')
 
-def plot_difference():
-    for folder in GOLD_FOLDERS:
-        gold_dir = os.path.abspath(
-            os.path.join(SMRF_FOLDER, 'tests', 'RME', folder)
-        )
+    results = gc.compare()
 
-        for gold_file in glob(gold_dir + '/*.nc'):
-            branch_data = get_file_values(CURRENT_BRANCH, gold_file)
-            master_data = get_file_values(MASTER_BRANCH, gold_file)
-            file_name = PurePath(gold_file).name
-
-            difference = branch_data - master_data
-            plt.figure()
-            plt.xlim(-.01, .01)
-            plt.hist(
-                difference,
-                label=LABEL.format(
-                    difference.min(),
-                    difference.max(),
-                    difference.mean()
-                ),
-                bins=numpy.arange(-.5, .5, .001),
-            )
-            plt.title(f"{folder.replace('_', ' ').upper()}\n {file_name}")
-            plt.legend()
-            plt.savefig(f"{folder}_hist_{file_name}.png")
+    gc.plot_results(results, plot_original_data=True)
 
 
 if __name__ == '__main__':
-    SMRF_REPO.checkout(CURRENT_BRANCH)
-    plot_difference()
-    SMRF_REPO.checkout(CURRENT_BRANCH)
+    main()
+    # SMRF_REPO.checkout(CURRENT_BRANCH)
+    # plot_difference()
+    # SMRF_REPO.checkout(CURRENT_BRANCH)
