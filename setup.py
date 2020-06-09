d9d5f09084d5c520831a897ca6b9fd904690ced2
Scott Havens <scott.havens@ars.usda.gov>
2020-06-09 15:50:38 -0600

    adding bibtex to extras require

--- setup.py
+++ setup.py
@@ -152,5 +152,8 @@ setup(
         'scripts/run_smrf',
         'scripts/mk_project',
         'scripts/gen_maxus'
-    ]
+    ],
+    extras_require={
+        'docs': ['sphinxcontrib-bibtex', 'sphinxcontrib-websupport']
+    }
 )
