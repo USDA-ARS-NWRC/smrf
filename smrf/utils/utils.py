e877a0d400750e0cf06a9199b11cf89b929d56f3
Scott Havens <scotthavens42@gmail.com>
2020-06-10 15:01:16 -0600

    made the time zone a raw string, fixing issue #96

--- smrf/utils/utils.py
+++ smrf/utils/utils.py
@@ -48,6 +48,26 @@ class CheckStation(CheckType):
         return value.upper()
 
 
+class CheckRawString(CheckType):
+    """
+    Custom `inicheck` checker that will not change the input string
+    """
+
+    def __init__(self, **kwargs):
+        super(CheckRawString, self).__init__(**kwargs)
+
+    def type_func(self, value):
+        """
+        Do not change the passed value at all
+        Args:
+            value: A single string
+        Returns:
+            value: A single string unchanged
+        """
+
+        return value
+
+
 def find_configs(directory):
     """
     Searches through a directory and returns all the .ini fulll filenames.
