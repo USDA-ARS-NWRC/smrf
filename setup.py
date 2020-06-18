c1cca42b79b2a0a0f87f77254d7ef9d000641fcd
Scott Havens <scotthavens42@gmail.com>
2020-06-18 15:44:04 -0600

    added importlib metadata to requirements, not fully sure if it needs to be there

--- setup.py
+++ setup.py
@@ -7,45 +7,6 @@ import numpy
 from Cython.Distutils import build_ext
 from setuptools import Extension, setup, find_packages
 
-# if sys.argv[-1] != 'test':
-
-#     # Grab and write the gitVersion from 'git describe'.
-#     gitVersion = ''
-#     gitPath = ''
-
-#     # get git describe if in git repository
-#     print('Fetching most recent git tags')
-#     if os.path.exists('./.git'):
-#         try:
-#             # if we are in a git repo, fetch most recent tags
-#             check_output(["git fetch --tags"], shell=True)
-#         except Exception as e:
-#             print('Unable to fetch most recent tags')
-
-#         try:
-#             ls_proc = check_output(["git describe --tags"], shell=True,
-#                                    universal_newlines=True)
-#             gitVersion = ls_proc
-#             print('Checking most recent version')
-#         except Exception as e:
-#             print('Unable to get git tag and hash')
-#     # if not in git repo
-#     else:
-#         print('Not in git repository')
-#         gitVersion = ''
-
-#     # get current working directory to define git path
-#     gitPath = os.getcwd()
-
-#     # git untracked file to store version and path
-#     fname = os.path.abspath(os.path.expanduser('./smrf/utils/gitinfo.py'))
-
-#     with open(fname, 'w') as f:
-#         nchars = len(gitVersion) - 1
-#         f.write("__gitPath__='{0}'\n".format(gitPath))
-#         f.write("__gitVersion__='{0}'\n".format(gitVersion[:nchars]))
-#         f.close()
-
 
 def c_name_from_path(location, name):
     return os.path.join(location, name).replace('/', '.')
