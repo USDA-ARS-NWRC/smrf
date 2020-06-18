ba3cfe2977bb60a3855108bb5cce730714fa172a
Scott Havens <scotthavens42@gmail.com>
2020-06-18 15:37:06 -0600

    added setuptools scm, added to smrf.__init__ to get a __version__ on runtime, removed some git stuff and mk_project

--- setup.py
+++ setup.py
@@ -2,51 +2,49 @@
 # -*- coding: utf-8 -*-
 
 import os
-import sys
-from subprocess import check_output
 
 import numpy
 from Cython.Distutils import build_ext
 from setuptools import Extension, setup, find_packages
 
-if sys.argv[-1] != 'test':
+# if sys.argv[-1] != 'test':
 
-    # Grab and write the gitVersion from 'git describe'.
-    gitVersion = ''
-    gitPath = ''
+#     # Grab and write the gitVersion from 'git describe'.
+#     gitVersion = ''
+#     gitPath = ''
 
-    # get git describe if in git repository
-    print('Fetching most recent git tags')
-    if os.path.exists('./.git'):
-        try:
-            # if we are in a git repo, fetch most recent tags
-            check_output(["git fetch --tags"], shell=True)
-        except Exception as e:
-            print('Unable to fetch most recent tags')
+#     # get git describe if in git repository
+#     print('Fetching most recent git tags')
+#     if os.path.exists('./.git'):
+#         try:
+#             # if we are in a git repo, fetch most recent tags
+#             check_output(["git fetch --tags"], shell=True)
+#         except Exception as e:
+#             print('Unable to fetch most recent tags')
 
-        try:
-            ls_proc = check_output(["git describe --tags"], shell=True,
-                                   universal_newlines=True)
-            gitVersion = ls_proc
-            print('Checking most recent version')
-        except Exception as e:
-            print('Unable to get git tag and hash')
-    # if not in git repo
-    else:
-        print('Not in git repository')
-        gitVersion = ''
+#         try:
+#             ls_proc = check_output(["git describe --tags"], shell=True,
+#                                    universal_newlines=True)
+#             gitVersion = ls_proc
+#             print('Checking most recent version')
+#         except Exception as e:
+#             print('Unable to get git tag and hash')
+#     # if not in git repo
+#     else:
+#         print('Not in git repository')
+#         gitVersion = ''
 
-    # get current working directory to define git path
-    gitPath = os.getcwd()
+#     # get current working directory to define git path
+#     gitPath = os.getcwd()
 
-    # git untracked file to store version and path
-    fname = os.path.abspath(os.path.expanduser('./smrf/utils/gitinfo.py'))
+#     # git untracked file to store version and path
+#     fname = os.path.abspath(os.path.expanduser('./smrf/utils/gitinfo.py'))
 
-    with open(fname, 'w') as f:
-        nchars = len(gitVersion) - 1
-        f.write("__gitPath__='{0}'\n".format(gitPath))
-        f.write("__gitVersion__='{0}'\n".format(gitVersion[:nchars]))
-        f.close()
+#     with open(fname, 'w') as f:
+#         nchars = len(gitVersion) - 1
+#         f.write("__gitPath__='{0}'\n".format(gitPath))
+#         f.write("__gitVersion__='{0}'\n".format(gitVersion[:nchars]))
+#         f.close()
 
 
 def c_name_from_path(location, name):
@@ -150,7 +148,6 @@ setup(
     scripts=[
         'scripts/update_configs',
         'scripts/run_smrf',
-        'scripts/mk_project',
         'scripts/gen_maxus'
     ],
     extras_require={
@@ -159,5 +156,11 @@ setup(
             'sphinxcontrib-websupport',
             'pydata-sphinx-theme'
         ]
-    }
+    },
+    use_scm_version={
+        'local_scheme': 'node-and-date',
+    },
+    setup_requires=[
+        'setuptools_scm'
+    ],
 )
--- smrf/__init__.py
+++ smrf/__init__.py
@@ -2,37 +2,48 @@
 '''
 SMRF - Snow Modeling Resource Framework
 '''
-__version__ = '0.9.1'
 
+from importlib_metadata import version, PackageNotFoundError
 import os
 
-__core_config__ = os.path.abspath(os.path.dirname(__file__) + '/framework/CoreConfig.ini')
-__recipes__ = os.path.abspath(os.path.dirname(__file__) + '/framework/recipes.ini')
-__config_changelog__ = os.path.abspath(os.path.dirname(__file__) + '/framework/changelog.ini')
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    # package is not installed
+    pass
 
+__core_config__ = os.path.abspath(
+    os.path.dirname(__file__) + '/framework/CoreConfig.ini')
+__recipes__ = os.path.abspath(os.path.dirname(
+    __file__) + '/framework/recipes.ini')
+__config_changelog__ = os.path.abspath(
+    os.path.dirname(__file__) + '/framework/changelog.ini')
 
 __config_titles__ = {
-                "topo":"Files for DEM and vegetation",
-                "time": "Dates to run model",
-                "stations":"Stations to use",
-                "csv":"CSV section configurations",
-                "mysql":"MySQL database",
-                "gridded":"Gridded datasets configurations",
-                "air_temp":"Air temperature distribution",
-                "vapor_pressure":"Vapor pressure distribution",
-                "wind": "Wind speed and wind direction distribution",
-                "precip": "Precipitation distribution",
-                "albedo":"Albedo distribution",
-                "cloud_factor": "Cloud Factor - Fraction used to limit solar radiation Cloudy (0) - Sunny (1)",
-                "solar":"Solar radiation",
-                "thermal":"Incoming thermal radiation",
-                "soil_temp":"Soil temperature",
-                "output":"Output variables",
-                "system":"System variables and Logging"
+    "topo": "Files for DEM and vegetation",
+    "time": "Dates to run model",
+    "stations": "Stations to use",
+    "csv": "CSV section configurations",
+    "mysql": "MySQL database",
+    "gridded": "Gridded datasets configurations",
+    "air_temp": "Air temperature distribution",
+    "vapor_pressure": "Vapor pressure distribution",
+    "wind": "Wind speed and wind direction distribution",
+    "precip": "Precipitation distribution",
+    "albedo": "Albedo distribution",
+    "cloud_factor": "Cloud Factor - Fraction used to limit solar radiation Cloudy (0) - Sunny (1)",
+    "solar": "Solar radiation",
+    "thermal": "Incoming thermal radiation",
+    "soil_temp": "Soil temperature",
+    "output": "Output variables",
+    "system": "System variables and Logging"
 }
 
-from . import data, distribute, envphys, framework, output, spatial, utils # isort:skip
+# from . import data, distribute, envphys, framework, output, spatial, utils  # isort:skip
 
-__config_header__ = utils.utils.getConfigHeader()
+__config_header__ = "Config File for SMRF {0}\n" \
+                    "For more SMRF related help see:\n" \
+                    "{1}".format(
+                        __version__, 'http://smrf.readthedocs.io/en/latest/')
 __config_checkers__ = 'utils.utils'
 # -*- coding: utf-8 -*-
