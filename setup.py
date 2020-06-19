e907d546ba13b79c3e26351c2a8328d88067327b
Scott Havens <scotthavens42@gmail.com>
2020-06-19 11:46:16 -0600

    removing python 3.5 support, will have to used ipw based smrf on older machines

--- setup.py
+++ setup.py
@@ -98,7 +98,6 @@ setup(
         'License :: OSI Approved :: CC0 1.0',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8'
