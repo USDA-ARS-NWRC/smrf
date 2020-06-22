81d406ddb7f4066c35b2a9a1e88b4820d6343435
Scott Havens <scotthavens42@gmail.com>
2020-06-22 16:40:10 -0600

    some flake8 but now allowing flake8 to fail

--- smrf/utils/__init__.py
+++ smrf/utils/__init__.py
@@ -1,2 +1,2 @@
 # -*- coding: utf-8 -*-
-from . import queue, utils, wind
+from . import queue, utils, wind  # noqa
--- smrf/utils/wind/__init__.py
+++ smrf/utils/wind/__init__.py
@@ -1,2 +1,2 @@
 # -*- coding: utf-8 -*-
-from . import model, wind_c
+from . import model, wind_c  # noqa
