9fe8fc289c355def3983d3c3f86a0c133b09da8d
Scott Havens <scotthavens42@gmail.com>
2020-06-23 16:39:20 -0600

    script loader is broader to work with local dev, docker and wheel builder

--- smrf/tests/scripts/script_test_helper.py
+++ smrf/tests/scripts/script_test_helper.py
@@ -1,21 +1,37 @@
 import os
+import shutil
 from importlib.machinery import SourceFileLoader
 from importlib.util import module_from_spec, spec_from_loader
 
 
 def create_script_spec(script_name):
 
-    # This path doesn't exist in docker or if it's installed as a package
-    SMRF_SCRIPT_HOME = os.path.abspath(
-        __file__ + '../../../../../') + '/scripts/'
-    if not os.path.exists(SMRF_SCRIPT_HOME):
-        SMRF_SCRIPT_HOME = '/usr/local/bin/'
+    # There are 3 places that the scripts can live
+    # 1. In the dev environment
+    # 2. In the docker image
+    # 3. Installed through wheel
+    SMRF_SCRIPT_HOME = [
+        os.path.abspath(__file__ + '../../../../../') + '/scripts/',
+        '/usr/local/bin/'
+    ]
+
+    found = False
+    for sch in SMRF_SCRIPT_HOME:
+        filename = os.path.join(sch, script_name)
+        if os.path.exists(filename):
+            SMRF_SCRIPT_HOME = sch
+            found = True
+            break
+
+    # if it wasn't found then it should be installed
+    if not found:
+        SMRF_SCRIPT_HOME = shutil.which(script_name)
 
     return spec_from_loader(
         script_name,
         SourceFileLoader(
             script_name,
-            SMRF_SCRIPT_HOME + script_name
+            SMRF_SCRIPT_HOME
         )
     )
 
