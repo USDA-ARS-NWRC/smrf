ffd1ca8b276666b31fda736f1113557d86f03103
Scott Havens <scotthavens42@gmail.com>
2020-07-07 15:01:45 -0600

    order import for data

--- smrf/data/__init__.py
+++ smrf/data/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # flake8: noqa
 from .csv import LoadCSV
-from .hrrr_grib import LoadGribHRRR
-from .load_data import LoadData
-from .load_topo import Topo
-from .netcdf import LoadNetcdf
 from .wrf import LoadWRF, metadata_name_from_index
+from .hrrr_grib import LoadGribHRRR
+from .netcdf import LoadNetcdf
+from .load_topo import Topo
+from .load_data import LoadData
