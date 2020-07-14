edf06d89cc445f829ddde45c1347c9758431077f
Joachim Meyer <j.meyer@utah.edu>
2020-07-14 15:12:55 -0600

    SMRFTestCaseLakes - Use constant.

--- smrf/tests/smrf_test_case_lakes.py
+++ smrf/tests/smrf_test_case_lakes.py
@@ -9,5 +9,5 @@ class SMRFTestCaseLakes(SMRFTestCase):
     """
 
     basin_dir = SMRFTestCase.test_dir.joinpath('basins', 'Lakes')
-    config_file = os.path.join(basin_dir, 'config.ini')
+    config_file = os.path.join(basin_dir, SMRFTestCase.BASE_INI_FILE_NAME)
     gold_dir = basin_dir.joinpath('gold_hrrr')
